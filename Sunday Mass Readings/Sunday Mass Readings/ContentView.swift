207b3733c2c3343473b3d363d10e1a3134d22a8b
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2023-06-26 15:38:09 -0600

    Expandable list created and styled

--- Sunday Mass Readings/Sunday Mass Readings/ContentView.swift
+++ Sunday Mass Readings/Sunday Mass Readings/ContentView.swift
@@ -8,15 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let Sundays : [Sunday] = [.january, .february, .march, .april, .may, .june, .july, .august, .september, .october, .november, .december]
+    let sampleMass : Mass = Mass.sampleMass.first! // content from json
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-        .preferredColorScheme(.dark)
+        NavigationStack {
+            VStack(alignment: .center) {
+                Spacer()
+                List(Sundays, children: \.sundays) { mass in
+                    if(!mass.isExpandable){
+                        NavigationLink{
+                            // dest
+                            Text(mass.name)
+                        }label: {
+                            // origin
+                            HStack{
+                                Image(systemName: "circle.fill")
+                                    .foregroundColor(mass.iconColor ?? .secondary)
+                                /*@START_MENU_TOKEN@*/Text(mass.name)/*@END_MENU_TOKEN@*/
+                            } // HS
+                        }
+                    }else{
+                        Text(mass.name) // months non-expandable
+                    }
+                } //Lst
+                .padding([.top, .bottom])
+                Spacer()
+                Text("year 2023 AD")
+                    .font(.footnote).bold()
+                    .padding(.bottom)
+            } //vs
+            .preferredColorScheme(.dark)
+            .toolbar{
+                ToolbarItem(placement: .principal) {
+                    VStack{
+                        Text("Sunday Mass")
+                            .font(.title2).bold()
+                            .padding(.top)
+                        Text("Readings")
+                            .font(.title3).bold()
+                            .padding(.bottom)
+                    }
+                }
+            } //.tool
+            .padding(.top)
+            .padding(.bottom, 20)
+        } // NV
+        .statusBarHidden(true)
+        .ignoresSafeArea()
     }
 }
 
